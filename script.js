74277e8ef2fe4168a606ae9090a4999de731ca11
Mohammed Vohra <90204393+VOHRAMHMD@users.noreply.github.com>
2026-02-13 01:34:33 +0100

    yes

--- script.js
+++ script.js
@@ -189,9 +189,9 @@ function drawText() {
         context.fillStyle = `rgba(255, 105, 180, ${opacity})`;
 
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["I love you so much {name}, more than", "all the time and space in the universe can contain"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["I love you so much Shruti, more than", "all the time and space in the universe can contain"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("I love you so much {name}, more than all the time and space in the universe can contain", canvas.width/2, canvas.height/2);
+            context.fillText("I love you so much Shruti, more than all the time and space in the universe can contain", canvas.width/2, canvas.height/2);
         }
 
         opacity = opacity + 0.01;
